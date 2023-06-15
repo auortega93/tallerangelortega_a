c784f9a880ab497fcb642d8424cf4c71bcaed588
auortega93 <135991022+auortega93@users.noreply.github.com>
2023-06-15 14:52:52 +0000

    implementacion de modulos

---
+++ programa.py
@@ -0,0 +1,9 @@
+from matematicas import sumar, restar,multiplicar,dividir
+from algebra import distancia
+
+
+print(sumar(2,3))
+print(restar(4,2))
+print(multiplicar(2,3))
+print(dividir(6,2))
+print(distancia(1,2,3,4))
